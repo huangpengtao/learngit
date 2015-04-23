f076d8df53acf483ece798caceb5fd25b5698d4a
Pengtao Huang <huangpengtao@huawei.com>
2015-04-23 22:45:17 +0800

    add me.py

---
+++ me.py
@@ -0,0 +1,10 @@
+'''
+Created on Apr 23, 2015
+
+@author: ff
+'''
+class TestCase(object):
+    def __init__(self, methodName='runTest'):
+        print 'ok'    
+if __name__ == '__main__':
+    TestCase()
