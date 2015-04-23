1de4ed0be17a80e2b93cd6a7adf5b88fdd7a3613
Pengtao Huang <huangpengtao@huawei.com>
2015-04-23 22:48:43 +0800

    copy test

--- me.py
+++ me.py
@@ -6,5 +6,6 @@ Created on Apr 23, 2015
 class TestCase(object):
     def __init__(self, methodName='runTest'):
         print 'ok'    
+        print 'ok'    
 if __name__ == '__main__':
     TestCase()
