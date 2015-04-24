4b88ee71c96778381f62dc259e100e73105a1af2
huangpengtao <huangpengtao@huawei.com>
2015-04-24 10:24:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,4 +6,9 @@ class Scheduler(object):
         print "test"
 if __name__ == '__main__':
     Scheduler()
+
 https://github.com/pyKun/vimrc/blob/master/git_script.sh
+git mv application.py newApplication.py
+git status
+
+git commit -m "Renamed git_script.sh  to git_init.sh"
