6c4b423dd660afc8b0e033e9eea949f76cdf72fd
huangpengtao <huangpengtao@huawei.com>
2015-05-07 16:51:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-class Scheduler(object):
-    """The base class that all Scheduler classes should inherit from."""
-
-    def __init__(self):
-        print "test"
-        print "test"
-if __name__ == '__main__':
-    Scheduler()
-
-https://github.com/pyKun/vimrc/blob/master/git_script.sh
-git mv application.py newApplication.py
-git status
-
-git commit -m "Renamed git_script.sh  to git_init.sh"
+ the detailed progress of OpenStack installations and configurations triggered by devstack
+devstack/localrc
+SCREEN_LOGDIR=$DEST/logs/screen  // log files of individual OpenStack /opt/stack/logs/screen/*.log
+LOGFILE=/opt/stack/logs/stack.sh.log
+VERBOSE=True
