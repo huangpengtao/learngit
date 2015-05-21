be8381e6ea6037708182ca50f7de8f6c459a1d10
huangpengtao <huangpengtao@huawei.com>
2015-05-21 10:13:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,8 @@ devstack/localrc
 SCREEN_LOGDIR=$DEST/logs/screen  // log files of individual OpenStack /opt/stack/logs/screen/*.log
 LOGFILE=/opt/stack/logs/stack.sh.log
 VERBOSE=True
+
+https://gerrit.googlecode.com/files/gerrit-2.2.2.war
+https://gerrit-releases.storage.googleapis.com/index.html
+https://gerrit-review.googlesource.com/Documentation/install-quick.html
+
