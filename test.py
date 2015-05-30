8eae4585213bb6932f5020a5862dfff87d84f442
huangpengtao <huangpengtao@huawei.com>
2015-05-30 14:49:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 http://wangchunfeng192.blog.163.com/blog/static/43280937201482510824346/
 
+http://longgeek.com/2013/12/26/ci-system-structures-iii-gerrit-installation-configuration/
+http://my.oschina.net/zhongl/blog/33017
+http://www.360doc.com/content/14/0417/11/14797374_369707389.shtml
+http://longgeek.com/2013/12/26/ci-system-structures-iii-gerrit-installation-configuration/
+
 
