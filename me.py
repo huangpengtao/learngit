d83ea79a1927fc0d874275b4e09c930ceac3a6f4
huangpengtao <huangpengtao@huawei.com>
2015-06-01 16:20:12 +0800

    Update me.py

--- me.py
+++ me.py
@@ -1,22 +1,24 @@
-'''
-Created on Apr 23, 2015
+http://dachary.org/?p=1716
+http://codexc.com/blog/2014/09/how-to-installation-and-configuration-gerrit-v2-10-with-github-oauth/
+apt-get install gitweb
+mkdir ~/gerrit/
+wget https://gerrit-releases.storage.googleapis.com/gerrit-2.10-rc0.war
 
-@author: ff
-'''
-class TestCase2(object):
-    def __init__(self):
-        print 'ok'
-    def bubble_sort(self,sort_list):
-        iter_len = len(sort_list)
-        if iter_len < 2:
-            return sort_list
-        for i in range(iter_len-1):
-            for j in range(iter_len-i-1):
-                if sort_list[j] > sort_list[j+1]:
-                    sort_list[j], sort_list[j+1] = sort_list[j+1], sort_list[j]
-        return sort_list
-if __name__ == '__main__':
-    bubble_test=TestCase2()
-    sort_list=[33,3,42,1,45,6]
-    ret=bubble_test.bubble_sort(sort_list)
-    print ret
+CREATE USER 'gerrit'@'localhost' IDENTIFIED BY 'yourpassword';
+CREATE DATABASE reviewdb;
+GRANT ALL ON reviewdb.* TO 'gerrit'@'localhost';
+FLUSH PRIVILEGES;
+exit;
+java -jar gerrit*.war init -d ~/gerrit/
+
+Database server type           [H2/?]: mysql
+atabase name                  [reviewdb]:
+Database username              [gerrit2]: gerrit
+gerrit2's password             : yourpassword
+Authentication method          [OPENID/?]: http
+Get username from custom HTTP header [y/N]? y
+Behind reverse proxy           [y/N]? y
+Proxy uses SSL (https://)      [y/N]? n
+anonical URL                  [http://review.probam.net/]: https://your-domain.com/
+java -jar gerrit*.war reindex
+
