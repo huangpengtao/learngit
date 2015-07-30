3e05344790038684370b56a65dbf4b9413cbfb6a
huangpengtao <huangpengtao@huawei.com>
2015-07-30 09:03:07 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,6 @@ http://my.oschina.net/zhongl/blog/33017
 http://www.360doc.com/content/14/0417/11/14797374_369707389.shtml
 http://longgeek.com/2013/12/26/ci-system-structures-iii-gerrit-installation-configuration/
 
+git config --global http.sslverify True
+git config --global  remote.gerrit.url https://huangpengtao:ba9kGdUqzKo+@review.openstack.org/openstack/nova.git
 
