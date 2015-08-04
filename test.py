faf9257025174750b9c38283b8a389c5907e7ffd
huangpengtao <huangpengtao@huawei.com>
2015-08-04 10:11:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,3 +10,16 @@ git config --global  remote.gerrit.url https://huangpengtao:ba9kGdUqzKo+@review.
 
 https://www.irccloud.com/pastebin/pJ1buYbh/
 https://www.irccloud.com/pastebin/nyTwAoSl/
+
+
+git review -d +你这个patch的网址的那个号
+git commit -a --amend
+git checkout -b all_mappings
+http://blog.csdn.net/lynn_kong/article/details/9405567
+冲突解决：
+git pull在master上
+在分支上
+git rebase master
+
+https://review.openstack.org/208511
+https://github.com/openstack/nova/blob/master/nova/tests/unit/compute/test_compute.py
